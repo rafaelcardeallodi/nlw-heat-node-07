4f91849bd9b1a742c54713d0d1d9a3bc3fd5ab37
rafaelcardeallodi <rafaelcardeallodi@hotmail.com>
2021-10-20 08:57:37 -0300

    Finalizado!

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import "dotenv/config";
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+import { Server } from 'socket.io';
 
 import { router } from './routes'
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
 app.use(express.json());
 
 app.use(router);
@@ -18,4 +36,4 @@ app.get('/signin/callback', (req, res) => {
   return res.json(code);
 })
 
-app.listen(3000, () => console.log('Server is running on PORT 3000!'));
+export { serverHttp, io };
